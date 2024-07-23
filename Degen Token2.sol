e962b8a22a423b3c9b4afd727e1b39b6ca655b41
Pragya69 <165126359+Pragya69@users.noreply.github.com>
2024-07-23 12:44:32 +0530

    Update Degen Token2.sol

--- Degen Token2.sol
+++ Degen Token2.sol
@@ -1,43 +1,46 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.23;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, ERC20Burnable, Ownable{
-     string[] public myArray;
-    constructor() ERC20("DegenToken","DGN"){ myArray = new string[](4);
+contract DegenToken is ERC20, Ownable {
+    
+    uint256 public constant REDEMPTION_RATE = 100;
 
-        myArray[0] = "Item 1";
-        myArray[1] = "Item 2";
-        myArray[2] = "Item 3";
-        myArray[3] = "Item 4";}
+    mapping(address => uint256) public swordsOwned;
 
-    function mint(address recipient, uint256 amount) external onlyOwner{
-        _mint(recipient,amount);
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        _mint(msg.sender, 10 * (10 ** uint256(decimals())));
     }
 
-    function transfer(address recipient,uint256 amount) public override returns (bool){
-        require(amount > 0,"Amount must be greater than zero.");
-        _transfer(_msgSender(),recipient,amount);
-        return true;
+    function redeemSword(uint256 quantity) public {
+        uint256 cost = REDEMPTION_RATE * quantity;
+        require(balanceOf(msg.sender) >= cost, "Not enough tokens to redeem for a sword");
+
+        swordsOwned[msg.sender] += quantity;
+        _burn(msg.sender, cost);
     }
 
-    function redeem(uint256 itemId,uint256 amount) public payable  {
-          require(
-            bytes(myArray[itemId]).length > 0,
-            "Item does not exist"
-        );
-
-        require(
-            balanceOf(msg.sender) > 0,
-            "Insufficient balance"
-        );
-        _burn(_msgSender(),amount);
+    function checkSwordsOwned(address user) public view returns (uint256) {
+        return swordsOwned[user];
     }
 
-    function checkBalance(address account) external view returns (uint256){
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function checkBalance(address account) public view returns (uint256) {
         return balanceOf(account);
+    }
+
+    function burnTokens(uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to burn");
+        _burn(msg.sender, amount);
+    }
+
+    function transferTokens(address to, uint256 amount) public {
+        require(to != address(0), "Invalid address");
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to transfer");
+        _transfer(msg.sender, to, amount);
     }
 }
